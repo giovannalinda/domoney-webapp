27df1765409eca3cf3f962c14f25984fb49bf773
Giovanna Linda <euGiovannaSouza@gmail.com>
2022-01-12 23:57:53 -0300

    form

--- src/pages/SignIn/SignIn.styled.ts
+++ src/pages/SignIn/SignIn.styled.ts
@@ -1,6 +1,80 @@
 import styled from 'styled-components/macro'
 import { theme } from 'config'
 
-export const Test = styled.h1`
-  color: ${theme.colors.neutral[900]};
+export const Content = styled.div`
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+
+  fieldset {
+    border: 0;
+    margin: 5px 1px 5px 14px;
+  }
+
+  span {
+    color: ${theme.colors.red};
+    font-size: 10px;
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  button {
+    margin: 10px 0 0 15px;
+    font-size: 15px;
+    color: ${theme.colors.neutral[900]};
+    background: ${theme.colors.neutral[900]};
+    border-radius: 5px;
+    height: 50px;
+    width: 444px;
+    border: 0;
+    transition: 1s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    :hover {
+      opacity: 85%;
+    }
+  }
+`
+
+export const Password = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+
+  fieldset {
+    flex-direction: column;
+    display: flex;
+  }
+
+  button {
+    position: absolute;
+    height: 40px;
+    width: 30px;
+    background: none !important;
+    border: 0;
+    transition: 1s;
+    right: 0;
+    bottom: 0;
+    left: 410px;
+    top: 25px;
+
+    svg {
+      color: ${theme.colors.neutral[900]};
+      position: relative;
+      margin: -10px 0 0 -30px;
+
+      :hover {
+        opacity: 85%;
+      }
+    }
+  }
 `
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { FiEye } from 'react-icons/fi'
 import { TextField } from 'ui/components/TextField'
 
+import * as S from './SignIn.styled'
+
+type FormData = {
+  email: string
+  password: string
+}
+
 export function SignIn() {
+  const [textPass, setTextPass] = useState(true)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+
+  function onSubmit(data: FormData) {
+    // eslint-disable-next-line no-console
+    console.log(data)
+  }
+
   return (
     <>
       <h1>Login</h1>
-      <form>
-        <TextField label='E-mail' />
-      </form>
+      <S.Content>
+        <S.Form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset>
+            <TextField
+              label='E-mail'
+              errorMessage={errors.email?.message}
+              placeholder='Seu melhor e-mail'
+              {...register('email', {
+                required: {
+                  value: true,
+                  message: 'Todos os campos são obrigatórios',
+                },
+              })}
+            />
+          </fieldset>
+          <fieldset>
+            <S.Password>
+              <TextField
+                label='Senha'
+                placeholder='Sua melhor senha'
+                type={textPass ? 'password' : 'text'}
+                {...register('password', {
+                  required: true,
+                })}
+              />
+              <button onClick={() => setTextPass(!textPass)} type='button'>
+                <FiEye />
+              </button>
+            </S.Password>
+          </fieldset>
+          <button type='submit'>Conectar-se</button>
+        </S.Form>
+      </S.Content>
     </>
   )
 }
